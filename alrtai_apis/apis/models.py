4c47669dc1c811da9d0ee558a0d83ca62369844f
agzuniverse <aswinganesh666@gmail.com>
2020-02-15 01:19:57 +0530

    Create model Client and register it in admin

--- alrtai_apis/apis/models.py
+++ alrtai_apis/apis/models.py
@@ -1,3 +1,7 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+
+class Client(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100)
