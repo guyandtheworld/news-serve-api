1f2369b1baffb548dbca1421816b879592e0edbe
agzuniverse <aswinganesh666@gmail.com>
2020-02-15 00:49:47 +0530

    Rename URL path to API from 'apis' to 'api'

--- alrtai_apis/alrtai_apis/urls.py
+++ alrtai_apis/alrtai_apis/urls.py
@@ -16,4 +16,4 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [path("admin/", admin.site.urls), path("apis/", include("apis.urls"))]
+urlpatterns = [path("admin/", admin.site.urls), path("api/", include("apis.urls"))]
