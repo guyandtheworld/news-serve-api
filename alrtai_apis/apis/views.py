3d8203fa5e36b38cf276a1038ab45aabb8539ef8
agzuniverse <aswinganesh666@gmail.com>
2020-02-15 01:21:09 +0530

    Created a placeholder view and url path to it

--- alrtai_apis/apis/views.py
+++ alrtai_apis/apis/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import views
+from rest_framework.response import Response
 
-# Create your views here.
+
+class Test(views.APIView):
+    def get(self, request):
+        return Response("TEST")
