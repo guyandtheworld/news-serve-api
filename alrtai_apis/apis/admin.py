eb79dec21f6900df09a2ecc121bf12b7f31715c3
agzuniverse <aswinganesh666@gmail.com>
2020-02-15 18:39:13 +0530

    Add users table and scenario table

--- alrtai_apis/apis/admin.py
+++ alrtai_apis/apis/admin.py
@@ -2,3 +2,6 @@ from django.contrib import admin
 from . import models
 
 admin.site.register(models.Client)
+admin.site.register(models.Scenario)
+admin.site.register(models.User)
+
--- alrtai_apis/apis/models.py
+++ alrtai_apis/apis/models.py
@@ -2,6 +2,28 @@ import uuid
 from django.db import models
 
 
+STATUSES = (("active", "Active"), ("demo", "Demo"), ("retired", "Retired"))
+
+
 class Client(models.Model):
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100)
+
+
+class Scenario(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100)
+    status = models.CharField(max_length=10, choices=STATUSES)
+    trackingDays = models.SmallIntegerField()
+
+
+class User(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=256)
+    status = models.CharField(max_length=10, choices=STATUSES)
+    clientID = models.ForeignKey("Client", on_delete=models.CASCADE)
+    defaultScenario = models.ForeignKey("Scenario", on_delete=models.CASCADE)
+    setupDate = models.DateField()
+
