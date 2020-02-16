a703acaa876ca502144071e6c92f0096b5bc9655
agzuniverse <aswinganesh666@gmail.com>
2020-02-16 14:47:58 +0530

    Prevent crashes when invalid post request is sent

--- alrtai_apis/apis/views.py
+++ alrtai_apis/apis/views.py
@@ -24,11 +24,13 @@ class GetUserUUID(views.APIView):
         return Response("Send POST request with JSON object with email as a key")
 
     def post(self, request):
-        email = request.data["email"]
-        user_uuid = User.objects.filter(email=email).first()
-        if user_uuid == None:
-            return Response({"success": False})
-        return Response({"success": True, "uuid": user_uuid.uuid})
+        if "email" in request.data:
+            email = request.data["email"]
+            user_uuid = User.objects.filter(email=email).first()
+            if user_uuid == None:
+                return Response({"success": False})
+            return Response({"success": True, "uuid": user_uuid.uuid})
+        return Response("Invalid object in request")
 
 
 class GetClientUUID(views.APIView):
@@ -36,8 +38,10 @@ class GetClientUUID(views.APIView):
         return Response("Send POST request with JSON object with uuid as a key")
 
     def post(self, request):
-        uuid = request.data["uuid"]
-        user = User.objects.filter(uuid=uuid).first()
-        if user == None:
-            return Response({"success": False})
-        return Response({"success": True, "uuid": user.clientID.uuid})
+        if "uuid" in request.data:
+            uuid = request.data["uuid"]
+            user = User.objects.filter(uuid=uuid).first()
+            if user == None:
+                return Response({"success": False})
+            return Response({"success": True, "uuid": user.clientID.uuid})
+        return Response("Invalid object in request")
