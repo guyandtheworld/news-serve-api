c14b9720bd8b5ac9c913eb593fa856dc50ecd290
agzuniverse <aswinganesh666@gmail.com>
2020-02-16 14:34:08 +0530

    Add endpoint to get UUID

--- alrtai_apis/apis/urls.py
+++ alrtai_apis/apis/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.Test.as_view(), name="index"),
+    path("getuuid", views.GetUUID.as_view()),
 ]
--- alrtai_apis/apis/views.py
+++ alrtai_apis/apis/views.py
@@ -1,8 +1,31 @@
+import json
 from django.shortcuts import render
+from django.http import JsonResponse
 from rest_framework import views
 from rest_framework.response import Response
+from .models.scenario import (
+    Scenario,
+    Bucket,
+    BucketWeight,
+    ModelDetails,
+    BucketModel,
+    Source,
+)
+from .models.users import User, Client, UserScenario
 
 
 class Test(views.APIView):
     def get(self, request):
         return Response("TEST")
+
+
+class GetUUID(views.APIView):
+    def get(self, request):
+        return Response("Send POST request with JSON object with email as a key")
+
+    def post(self, request):
+        email = request.data["email"]
+        user_uuid = User.objects.filter(email=email).first()
+        if user_uuid == None:
+            return Response({"success": False})
+        return Response({"success": True, "uuid": user_uuid.uuid})
