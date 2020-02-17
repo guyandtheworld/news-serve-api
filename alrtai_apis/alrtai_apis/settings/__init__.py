4a034bb5c124476470e1ab20123c12c06ec21267
Adarsh S <4darshofficial@gmail.com>
2020-02-17 22:35:42 +0530

    default init values

--- alrtai_apis/alrtai_apis/settings/__init__.py
+++ alrtai_apis/alrtai_apis/settings/__init__.py
@@ -3,7 +3,7 @@ from .common import *
 
 # you need to set "myproject = 'prod'" as an environment variable
 # in your OS (on which your website is hosted)
-if os.environ['RUN_TYPE'] == 'prod':
+if os.getenv('RUN_TYPE', 'dev') == 'prod':
    from .prod import *
 else:
    from .dev import *
