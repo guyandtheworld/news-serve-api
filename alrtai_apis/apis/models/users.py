039cae7dc3c071d0e36a1951e1b6b76432d8d712
agzuniverse <aswinganesh666@gmail.com>
2020-02-23 19:25:22 +0530

    Add foreign key from django auth's user to alrtai user

--- alrtai_apis/apis/models/users.py
+++ alrtai_apis/apis/models/users.py
@@ -1,5 +1,6 @@
 import uuid
 from django.db import models
+from django.contrib.auth.models import User
 
 STATUSES = (("active", "Active"), ("demo", "Demo"), ("retired", "Retired"))
 
@@ -13,11 +14,11 @@ class Client(models.Model):
 
 
 class User(models.Model):
+    user = models.ForeignKey("User", on_delete=models.CASCADE),
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4,
                             editable=False)
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
-    password = models.CharField(max_length=256)
     status = models.CharField(max_length=10, choices=STATUSES)
     clientID = models.ForeignKey("Client", on_delete=models.CASCADE)
     defaultScenario = models.ForeignKey("Scenario", on_delete=models.CASCADE)
