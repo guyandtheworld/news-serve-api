5d99ef12c978fbda53f1af3e4f0949edc4c3d485
Adarsh S <4darshofficial@gmail.com>
2020-05-25 17:33:28 +0530

    create story entity table

--- apps/feed/admin.py
+++ apps/feed/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
 from .models import (
+    StoryWarehouse,
     ClusterMap,
     Cluster
 )
 
+admin.site.register(StoryWarehouse)
 admin.site.register(ClusterMap)
 admin.site.register(Cluster)
--- apps/feed/models.py
+++ apps/feed/models.py
@@ -2,6 +2,9 @@ import uuid
 from django.db import models
 
 
+from django.contrib.postgres.fields import JSONField
+
+
 class Cluster(models.Model):
     """
     Table for storing each new entities and their
@@ -35,3 +38,33 @@ class ClusterMap(models.Model):
 
     def __str__(self):
         return "{}".format(self.storyID.title)
+
+
+class StoryWarehouse(models.Model):
+    """
+    Table for storing each stories and their
+    respective scores on the go.
+    """
+
+    uuid = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False)
+    story_id = models.UUIDField()
+    story_title = models.CharField(max_length=2000)
+    story_url = models.CharField(max_length=1000)
+    published_date = models.DateTimeField()  # timezone localized date
+    domain = models.CharField(max_length=150)
+    language = models.CharField(max_length=100)
+    source_country = models.CharField(max_length=100)
+    entity_name = models.CharField(max_length=200)
+    entity_id = models.UUIDField()
+    scenario_id = models.UUIDField()
+    story_body = models.CharField(max_length=10000)
+    timestamp = models.DateTimeField()
+    scores = JSONField()  # gross score, source score
+    entities = JSONField()
+    hotness = JSONField()
+    bucket_score = JSONField()
+    sentiment = JSONField()
+
+    def __str__(self):
+        return "{}".format(self.story_title)
